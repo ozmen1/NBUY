39f542f04b6736f983cfe0b8521dd5092ee90af7
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-10-03 18:03:08 +0300

    Commit

---
+++ Project01_HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
