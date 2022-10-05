9a1bd17d972b3cc37b3bf9c1a3650fc27b117519
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-10-05 16:12:18 +0300

    day3

---
+++ Week_01/Project10_SwitchCondition/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Project10_SwitchCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Klavyeden girilen sayının 5 olması halinde 2 katını, 10 olması halinde 3 katını, 15 olması halinde 4 katını ekrana yazdıran. 
+            ////Eğer üçü de değilse tanınmayan değer yaz.
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //int sonuc = 0;
+            //if (input == 5)
+            //{
+            //    sonuc = input * 2;
+            //}
+            //else if(input == 10)
+            //{
+            //    sonuc = input * 3;
+            //}
+            //else if(input == 15)
+            //{
+            //    sonuc = input * 4;
+            //}
+            //if (sonuc == 0)
+            //{
+            //    Console.WriteLine("Tanımlanmamış değer!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(sonuc);
+            //}
+
+
+
+
+
+
+
+
+        }
+    }
+}
