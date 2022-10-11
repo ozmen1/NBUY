2df9ea95bb1c2ddc8c1b37d981c037cd23f40d58
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-10-11 13:22:32 +0300

    day7

---
+++ Week_02/Project04_Sayisal_Loto_Oyunu/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+
+namespace Project04_Sayisal_Loto_Oyunu
+{
+    internal class Program
+    {
+        static int[] SayiUret()
+        {
+            Random rnd = new Random();
+            int[] loto = new int[6];
+            int sayi;
+            for (int i = 0; i < 6; i++)
+            {
+                do
+                {
+                    sayi = rnd.Next(1, 50);
+                } while (loto.Contains(sayi));
+                loto[i] = sayi;
+            }
+            return loto;
+        }
+
+        static void Hile(int[] loto)
+        {
+            Console.WriteLine("HİLE");
+            Console.WriteLine("****");
+            foreach (var siradakiSayi in loto)
+            {
+                Console.WriteLine(siradakiSayi);
+            }
+        }
+
+        static int TahminYap(int tahminSiraNo) //kullanıcının her tahmin yapışını burası sağlaayacak.
+        {
+            int tahmin;
+            Console.Write($"{tahminSiraNo}. tahmininizi giriniz: ");
+            tahmin = int.Parse(Console.ReadLine());
+            return tahmin;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 1. sistem 6 tane farklı sayı üretsin (1-49 arasında)
+             * 2. kullanıcıdan 6 tane tahmin alalım. //1-49 arası dışındaki tahminleri kabul etme!
+             * 3. sonuç olarak kullanıcının kaç tane doğru tahmin yaptığını tahminleriyle birlikte yazdıralım.
+             * 4. not: hiç doğru tahmin yapamazsa kaybetiiniz yazınız.
+            */
+            int[] loto = SayiUret();
+            int[] tahminler = new int[6];
+            Hile(loto);
+            int tahmin;
+            for (int i = 0; i < 6; i++)
+            {
+                do
+                {
+                    tahmin = TahminYap(i + 1);
+                } while (tahmin < 0 || tahmin > 49);
+                tahminler[i] = tahmin;
+            }
+            //baştan kaç tane doğru sayı tahmin edileceği bilinemeyince arraylist tanımladık.
+            ArrayList bilinenler = new ArrayList(); //Arraylistlerin kaç elemanlı olacağı söylenmez! İçinde her bir eleman istenilen tipte değer tutabilir int, string, char vb.
+
+            foreach (var siradakiTahmin in tahminler)  
+            {
+                if(loto.Contains(siradakiTahmin))
+                {
+                    bilinenler.Add(siradakiTahmin); 
+                }
+            }
+            if (bilinenler.Count == 0)
+            {
+                Console.WriteLine("Kaybettiniz, hiç bilemediniz!");
+            }
+            else
+            {
+                Console.WriteLine($"Tebrikler, {bilinenler.Count} adet doğru tahmin yaptınız!");
+                Console.WriteLine("Bildiğiniz sayılar:");
+                foreach (var siradakiBilinen in bilinenler)
+                {
+                    Console.WriteLine(siradakiBilinen);
+                }
+            }
+
+            Console.ReadLine(); //ekran hızlıca kapanmasın 
+        }
+    }
+}
---
+++ Week_02/Project05_Methods_Advanced/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Project05_Methods_Advanced
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Year { get; set; }
+
+            public string Intro()
+            {
+                return $"Ad: {this.Name}, Yaş: {this.CalculateAge()}";  //this: nesnedeki Name'i alır
+            }
+
+            private int CalculateAge()
+            {
+                return DateTime.Now.Year - this.Year;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { Name = "Engin", Year = 1975 };
+            Person person2 = new Person() { Name = "Umay", Year = 2007 };
+            Person person3 = new Person() { Name = "Begüm", Year = 2000 };
+            Person person4 = new Person() { Name = "Ayşen", Year = 1985 };
+            Person person5 = new Person() { Name = "Ceyda", Year = 1990 };
+
+            Console.WriteLine(person1.Intro());
+
+            Console.ReadLine();
+        }
+    }
+}
