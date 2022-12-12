13754449f8e36cbd4ac076e9bad1fd31d05d3749
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-12-12 18:07:43 +0300

    day50

---
+++ Week_11/12-12-2022/ShoppingApp/ShoppingApp.Data/Concrete/EfCore/Repositories/EfCoreProductRepository.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Data.Abstract;
+using ShoppingApp.Data.Concrete.EfCore.Contexts;
+using ShoppingApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Concrete.EfCore.Repositories
+{
+    public class EfCoreProductRepository : EfCoreGenericRepository<Product>, IProductRepository
+    {
+        public EfCoreProductRepository(ShopAppContext context) : base(context)
+        {
+
+        }
+        private ShopAppContext ShopAppContext
+        {
+            get { return _context as ShopAppContext; }
+        }
+
+        public async Task CreateProductAsync(Product product, int[] selectedCategoryIds)
+        {
+            //Önce product kaydı yapacağız böylece elimizde product Id olacak. Sonra da bu product Id'yi kullanarak, productcategory kaydını/kayıtlarını yapacağız.
+
+
+            await ShopAppContext.Products.AddAsync(product);
+            await ShopAppContext.SaveChangesAsync(); //Id'si oluşuyor.
+            product.ProductCategories = selectedCategoryIds
+                .Select(catId => new ProductCategory
+                {
+                    ProductId = product.Id,
+                    CategoryId = catId
+                }).ToList();
+            await ShopAppContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetHomePageProductsAsync()
+        {
+            return await ShopAppContext
+                .Products
+                .Where(p => p.IsHome && p.IsApproved)
+                .ToListAsync();
+        }
+
+        public Task<Product> GetProductDetailsByUrlAsync(string productUrl)
+        {
+            return ShopAppContext
+                .Products
+                .Where(p => p.Url == productUrl)
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Category)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Product>> GetProductsByCategoryAsync(string category)
+        {
+            var products = ShopAppContext.Products.AsQueryable();
+            if (category != null)
+            {
+                products = products
+                    .Where(p => p.IsApproved)
+                    .Include(p => p.ProductCategories)
+                    .ThenInclude(pc => pc.Category) //pc=productcategory
+                    .Where(p => p.ProductCategories.Any(pc => pc.Category.Url == category));
+            }
+            return await products.ToListAsync();
+        }
+
+
+        public async Task<List<Product>> GetProductsWithCategories()
+        {
+            return await ShopAppContext
+                .Products //shopappcontext'ten git products'a eriş
+                .Include(p => p.ProductCategories) //productların içinde dön ve şu koşula uygun olanları al
+                .ThenInclude(p => p.Category) //product bilgilerine dahil et: kategoriyi
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProductWithCategories(int id)
+        {
+            return await ShopAppContext
+                .Products 
+                .Where(p=>p.Id == id)
+                .Include(p => p.ProductCategories) //her bir product için productscategories'e geç
+                .ThenInclude(p => p.Category) //her bir product categories'den de category'lerini dahil et.
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product, int[] selectedCategoryIds)
+        {
+            var newProduct = await ShopAppContext
+                .Products
+                .Include(p => p.ProductCategories)
+                .FirstOrDefaultAsync(p => p.Id == product.Id);
+            newProduct.ProductCategories = selectedCategoryIds
+                .Select(catId => new ProductCategory
+                {
+                    ProductId = newProduct.Id,
+                    CategoryId = catId
+                }).ToList();
+            ShopAppContext.Update(newProduct);
+            await ShopAppContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Week_11/12-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Business.Abstract;
+using ShoppingApp.Core;
+using ShoppingApp.Entity.Concrete;
+using ShoppingApp.Web.Areas.Admin.Models.Dtos;
+using System;
+
+namespace ShoppingApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public ProductController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProductsWithCategories();
+            var productListDto = products
+                .Select(p => new ProductListDto
+                {
+                    Product = p
+                }).ToList();
+            return View(productListDto);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var productAddDto = new ProductAddDto
+            {
+                Categories = categories
+            };
+            return View(productAddDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductAddDto productAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var url = Jobs.InitUrl(productAddDto.Name);
+                var product = new Product
+                {
+                    Name = productAddDto.Name,
+                    Price = productAddDto.Price,
+                    Description = productAddDto.Description,
+                    Url = url,
+                    IsApproved = productAddDto.IsApproved,
+                    IsHome = productAddDto.IsHome,
+                    ImageUrl = Jobs.UploadImage(productAddDto.ImageFile)
+                };
+                await _productService.CreateProductAsync(product, productAddDto.SelectedCategoryIds);
+                return RedirectToAction("Index");
+            }
+            var categories = await _categoryService.GetAllAsync();
+            productAddDto.Categories = categories;
+            return View(productAddDto);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var productsWithCategories = await _productService.GetProductWithCategories(id);
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            var productUpdateDto = new ProductUpdateDto
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Description = product.Description,
+                //Url = Jobs.InitUrl(product.Name),
+                IsApproved = product.IsApproved,
+                IsHome = product.IsHome,
+                //ImageUrl = product.ImageUrl
+                Categories = await _categoryService.GetAllAsync();
+            };
+            return View(productUpdateDto);
+
+
+    }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductUpdateDto productUpdateDto, int[] selectedCategoryIds)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = await _productService.GetByIdAsync(productUpdateDto.id);
+                if (product==null)
+                {
+                    return NotFound();
+                }
+            var url = Jobs.UploadImage(productUpdateDto.Name);
+            product.Name = productUpdateDto.Name;
+            product.Price = productUpdateDto.Price;
+            product.Description = productUpdateDto.Description;
+            product.IsApproved = productUpdateDto.IsApproved;
+            product.IsHome = productUpdateDto.IsHome;
+            product.ImageUrl = Jobs.UploadImage(productUpdateDto.ImageFile);
+            product.Url = url;
+            await _productService.UpdateProductAsync(product, selectedCategoryIds);
+            return RedirectToAction("Index");
+            }
+        return View();
+        }
+    }
+}
