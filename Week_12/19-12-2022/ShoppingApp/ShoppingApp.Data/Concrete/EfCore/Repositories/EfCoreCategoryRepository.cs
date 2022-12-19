7f40d6f08c037bfc53494c349e9242b4499f308c
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-12-19 18:02:23 +0300

    day55

---
+++ Week_12/19-12-2022/ShoppingApp/ShoppingApp.Data/Concrete/EfCore/Repositories/EfCoreCategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Data.Abstract;
+using ShoppingApp.Data.Concrete.EfCore.Contexts;
+using ShoppingApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Concrete.EfCore.Repositories
+{
+    public class EfCoreCategoryRepository : EfCoreGenericRepository<Category>, ICategoryRepository
+    {
+        public EfCoreCategoryRepository(ShopAppContext context) : base(context) //base'e gönderdik.
+        {
+            //buraya gelen context base class'a gönderiliyor ve aynı zamanda sadece bu constructor içinde geçerli. Ama bu class'ın bütününde henüz kullanılamıyor.Eğer kullanılsın istersek yapmamız gereken işlemler var.
+        }
+
+        public Category GetByIdWithProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
