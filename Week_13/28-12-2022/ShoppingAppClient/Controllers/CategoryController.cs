179a8a178dcb996baaae6e5f5d01b2c4689ca712
Kerem Özmen <44169946+ozmen1@users.noreply.github.com>
2022-12-28 15:30:16 +0300

    day62

---
+++ Week_13/28-12-2022/ShoppingAppClient/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoppingAppClient.Models;
+
+namespace ShoppingAppClient.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var kategoriler = new List<CategoryViewModel>();
+            using (var httpClient = new HttpClient()) //using=işi bittikten sonra bellekten yok olsun
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5200/api/categories/"))
+                //using (var response = await httpClient.GetAsync("http://shoppingapp.somee.com/api/categories"))
+                {
+                    //json formatını deserialize edip productviewmodel'e çevirelim.
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    kategoriler = JsonConvert.DeserializeObject<List<CategoryViewModel>>(apiResponse);
+                }
+            }
+            return View(kategoriler);
+        }
+    }
+}
